65662113878627b9e97c18c16f14f02534004fb6
JonSoft Solutions <jonathandev25@gmail.com>
2017-04-17 11:27:56 -0400

    done in class

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,11 @@
 module HomesHelper
+  def home_age_class(date)
+    if date < 30.days.ago
+      return "old-home"
+    elsif date >= 2.days.ago
+      return "new-home"
+    else
+      return "standard-home"
+    end
+  end
 end
