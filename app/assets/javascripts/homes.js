63777bd1c27bde3eb1d424fbc85dd88cabda73ea
JonSoft Solutions <jonathandev25@gmail.com>
2017-04-30 16:06:14 -0400

    for review

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+  $('#search').on('input', function(event) {
+    let queryValue = $(this).val()
+
+  console.log(`You are searching for ${queryValue}`)
+
+  $.ajax({
+    url: '/homes',
+    data: { search: searchValue },
+    dataType: 'script'
+  })
+})
